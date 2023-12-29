521692b3384a40b827833c99a5ab7918b2c24e2d
Md Asjad <mdasjad895@gmail.com>
2023-12-29 19:40:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from src.helper import download_hugging_face_embeddings
+# from src.helper import download_hugging_face_embeddings
 from langchain.vectorstores import Pinecone
 import pinecone
 import replicate
